e6aa30dc68f050e5c313bd833dd4df71abcfc8c2
Al3ksanderD <olo1045@wp.pl>
2023-04-15 15:13:02 +0200

    1

--- src/components/navbars/Navbar.js
+++ src/components/navbars/Navbar.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Logo from '../../assets/transLogo.png';
 
 export const Navbar = () => {
   return (
-    <div className='navbar__container'>
-        <div className='navbar__home'>
-            <Link to="/">Home</Link>
-        </div>
-        <div className='navbar_logo'>
-            Fitness Oasis
-        </div>
+    <header>
+    <div class="container">
+      <h1 class="logo"> Fitness Oasis</h1>
 
-        <div className='navbar__links'> 
-            
-            <Link to="/login">Login</Link>
-            <Link to="/singup">Sign up!</Link>
-        </div>
-
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/reservation">Reservations</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/singup">Singup</Link></li>
+        </ul>
+      </nav>
     </div>
+    </header>
+    // <div className='navbar__container'>
+    //   <div className='navbar__left'>
+    //     <div className='navbar__home'>
+    //         <Link to="/">HOME</Link>
+    //     </div>
+    //     <div className='navbar__reservations'>
+    //         <Link to="/reservations">My Reservations</Link>
+    //     </div>
+    //   </div>
+    //   <div className='navbar__center'>
+    //     <div className='navbar_logo'>
+    //         <img src={Logo} alt='Logo image'  />
+    //     </div>
+    //   </div>  
+        
+    //   <div className='navbar__right'>
+    //     <div className='navbar__login'> 
+    //         <Link to="/login">Login</Link>
+    //     </div>
+    //     <div className='navbar__singup'>    
+    //         <Link to="/singup">Sign up!</Link>
+    //     </div>
+    //   </div>
+        
+
+    // </div>
   )
 }
 export default Navbar
--- src/containers/homesite/HomeSite.js
+++ src/containers/homesite/HomeSite.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import Navbar from '../../components/navbars/Navbar'
+import React from 'react';
+import Navbar from '../../components/navbars/Navbar';
+import './HomeSite.css';
+import krakow from '../../assets/kraków.jpeg'
+import gdansk from '../../assets/gdańsk.jpeg'
+import warszawa from '../../assets/warszawa.jpeg'
+import logo from '../../assets/transLogo.png'
 
 export const HomeSite = () => {
   return (
     <div className='home__container'>
         <Navbar/>
-        <div>HomeSite</div>
+        <div className='home__content'>
+          <div className='home__content-welcome_sign'>
+            <div className='home__content-welcome_sign-text'>
+              <h1> Welcome To Fitness Oasis </h1>
+              <h2>Start by Picking a City</h2>
+            </div>
+            <div className='home__content-welcome_sign-img'>
+            <img src={logo} alt='city image'/>
+            </div>
+            
+          </div>
+          <div className='home__content-main grid-container'>
+            <div className='home__content-main_city'>
+              <img src={krakow} alt='city image'/>
+              <p className='home__content-text krakow'>KRAKÓW</p>
+            </div>
+            <div className='home__content-main_city'>
+              <img src={gdansk} alt='city image'/>
+              <p className='home__content-text gdansk'>GDAŃSK</p>
+            </div>
+            <div className='home__content-main_city'>
+              <img src={warszawa} alt='city image'/>
+              <p className='home__content-text warszawa'>WARSZAWA</p>
+            </div>
+          </div>
+        </div>
     </div>
     
   )
